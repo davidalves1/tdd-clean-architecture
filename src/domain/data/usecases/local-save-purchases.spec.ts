3fcd663a3ceed3be7b401b04fd25d875b6f6bf43
davidalves1 <alves.david@outlook.com>
2021-02-27 19:46:22 -0300

    ensure LocalSavePurchases does not delete cache  upon creation

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,5 +1,17 @@
-describe('', () => {
-  it('', () => {
-    expect(1).toBe(1);
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: CacheStore) {}
+}
+
+interface CacheStore {}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+}
+
+describe('LocalSavePurchases', () => {
+  it('should not delete cache on init', () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
   });
 });
